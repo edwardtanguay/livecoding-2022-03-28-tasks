448fbe435ad476b3bd4f745165dee70a10888bdb
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-28 13:48:11 +0200

    read attributes

--- js/changeBackground.js
+++ js/changeBackground.js
@@ -1,6 +1,8 @@
 const textElem = document.querySelector('.text'); 
 const messageElem = document.querySelector('.message'); 
+const linkElem = document.querySelector('.link');
 
 textElem.addEventListener('focus', () => {
 	messageElem.style.backgroundColor = 'yellow';
+	messageElem.innerText =`href=${linkElem.href} and target=${linkElem.target}`;
 });
