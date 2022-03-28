63572abb90bcd3e5629df7f76956af9b790707eb
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-28 11:46:24 +0200

    four event syntaxes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const buttonElem = document.querySelector('button');
+
+// buttonElem.style.display = 'none';
+
+// syntax #1
+buttonElem.addEventListener("click", () => {
+	console.log('clicked with anonymous arrow function');
+});
+
+// syntax #2
+buttonElem.addEventListener("click", function () {
+	console.log('clicked with anonymous classic function')
+});
+
+// syntax #3
+buttonElem.addEventListener("click", changeText);
+function changeText() {
+	console.log('clicked with named classic function');
+}
+
+// syntax #4
+const changeText2 = () => {
+	console.log('clicked with named arrow function');
+};
+buttonElem.addEventListener("click", changeText2);
